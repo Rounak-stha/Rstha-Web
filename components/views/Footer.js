7956d2aa1093fe03d065af4a48c06ef64f08669f
Rouak-stha <rounakshrestha1153@gmail.com>
2022-10-18 18:38:03 +0545

    Footer Update: Items aligned to start (Now do not take the whole width)

--- components/views/Footer.js
+++ components/views/Footer.js
@@ -9,13 +9,15 @@ const footerItems = navItems
 export default function Footer() {
   return (
     <div className='mb-2 mono'>
-      <div className='flex flex-col xs:h-36 flex-wrap w-full mx-auto text-gray-600 dark:text-gray-400'>
+      <div className='flex flex-col items-start xs:h-36 flex-wrap w-full mx-auto text-gray-600 dark:text-gray-400'>
         {footerItems.map(({ name, href }) => (
-          <Link key={name} href={href}>
-            <a className='mr-2 cursor-pointer hover:underline block p-1 sm:px-4 sm:py-1.5'>
-              {name}
-            </a>
-          </Link>
+          <div key={name}>
+              <Link href={href}>
+              <a className='mr-2 cursor-pointer hover:underline block p-1 sm:px-4 sm:py-1.5'>
+                {name}
+              </a>
+            </Link>
+          </div>
         ))}
       </div>
     </div>
