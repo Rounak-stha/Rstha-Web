1a81c7ae334d2214293b35c57884dd2dbc656792
Rouak-stha <rounakshrestha1153@gmail.com>
2023-02-06 15:23:29 +0545

    Profile Text Update

--- components/views/Profile.js
+++ components/views/Profile.js
@@ -7,9 +7,8 @@ export default function Profile() {
       <div className='mb-4'>
         <H3 className='mt-0'>
           Hey <span className='wave-emoji'>👋</span>, welcome! I am Rounak
-          Shrestha. I enjoy blogging, coding, playing table tennis, watching
-          Anime. I would love to know about you too. Write to me or lets
-          connect.
+          Shrestha. I do Full Stack Development. Things I enjoy includes enjoy blogging, coding, playing table tennis, watching
+          Anime. I would love to know about you too. Write to me or lets connect.
         </H3>
       </div>
       <SocialLinks center />
