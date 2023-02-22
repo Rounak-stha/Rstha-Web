8c089613f2f69bc38f27f0089f69562f87a341bb
Rouak-stha <rounakshrestha1153@gmail.com>
2023-02-22 11:38:53 +0545

    About Me Update

--- constants/texts.js
+++ constants/texts.js
@@ -4,8 +4,7 @@ export const ABOUT_ME = [
     `I wanted to start with a joke first but can't think of an original one. Intros talk about who a person is, so who am I? For those
     who are wondering ...`,
     `Long story short, I am a self-taught (taught by awesome online resources) programmer who
-    enjoys exploring and learning about CS`,
-    `And yes, I have a Bachelor's Degree, just not in CS`
+    enjoys exploring and learning about CS`
 ]
 
 export const REACH_OUT = 'I am always open to conversations, be it for any tech help, project partnership or just some friendly debate on relatable topics.'
