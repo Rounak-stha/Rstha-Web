2e336f99e80db6cb52b0ccb3e864d8d105b70c24
Rouak-stha <rounakshrestha1153@gmail.com>
2023-03-22 11:47:09 +0545

    Updated Blog Footer Audience Msg

--- components/views/Blog.js
+++ components/views/Blog.js
@@ -12,29 +12,36 @@ import { P3 } from '../Typography/ParaGraph';
 // https://mdxjs.com/
 
 const components = {
-  HighLight,
-  ImageWithCaption,
-  ImageFlexBox,
-  Callout,
-  Math,
-  Caption
+    HighLight,
+    ImageWithCaption,
+    ImageFlexBox,
+    Callout,
+    Math,
+    Caption
 };
 
 export default function Blog({ blogData }) {
-  const { title, created, readingTime, data } = blogData;
-  return (
-    <>
-      <Head>
-        <title>{title}</title>
-      </Head>
-      <article>
-        <H1 className='text-xl sm:text-4xl font-[800] mt-0 mb-2'>{title}</H1>
-        <P3 className='mt-0 mb-4'>
-          {created} . {readingTime} min read
-        </P3>
-        <MDXRemote {...data} components={components} />
-        <p className='m-0'>If found any error or mistake in this blog post, please do not hesitate to <a className='italic font-bold' href='mailto:rsthaofficial@gmail.com'>reach out</a> to me.</p>
-      </article>
-    </>
-  );
+    const { title, created, readingTime, data } = blogData;
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <article>
+                <H1 className='text-xl sm:text-4xl font-[800] mt-0 mb-2'>{title}</H1>
+                <P3 className='mt-0 mb-4'>
+                    {created} . {readingTime} min read
+                </P3>
+                <MDXRemote {...data} components={components} />
+                <p className='m-0'>
+                    Hey I assume you finished reading, I would love to know your feedback or if found any error or
+                    mistake in this blog post, please do not hesitate to{' '}
+                    <a className='italic font-bold' href='mailto:rsthaofficial@gmail.com'>
+                        reach out
+                    </a>{' '}
+                    to me.
+                </p>
+            </article>
+        </>
+    );
 }
