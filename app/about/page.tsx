776246b3c87f407d23a9482c3040b615156f2448
Rouak-stha <rounakshrestha1153@gmail.com>
2023-07-08 19:37:43 +0545

    Removed Tweets and Tracks

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,7 @@
-import { Suspense } from 'react'
-import CurrentlyPlaying from '@/components/views/CurrentlyPlaying'
-import TweetSkeleton from '@/components/views/TweetSkeleton'
-import { H2, H4 } from '@/components/Typography/Headings'
+import { H2 } from '@/components/Typography/Headings'
 import { ABOUT_ME, REACH_OUT } from '../../constants'
 import SocialLinks from '@/components/views/SocialLinks'
 import { P2 } from '@/components/Typography/ParaGraph'
-import Tracks from './components/Tracks'
-import LikedTweets from './components/LikedTweets'
-import RecentTweets from './components/RecentTweets'
 import Projects from './components/Projects'
 
 export const metadata = {
@@ -31,29 +25,6 @@ export default function More() {
             <div id="projects" className="mb-8">
                 <H2>Recent Projects</H2>
                 <Projects />
-            </div>
-            <div className="mb-8">
-                <div className="flex items-center flex-wrap">
-                    <H2 className="mr-2 !my-0">Music</H2>
-                    <CurrentlyPlaying />
-                </div>
-                <H4 className="my-1">Tracks on Repeat</H4>
-                {/* @ts-expect-error Server Component */}
-                <Tracks />
-            </div>
-            <div className="mb-8" id="recent_tweets">
-                <H2 className="font-semibold">Recent Tweets</H2>
-                <Suspense fallback={<TweetSkeleton />}>
-                    {/* @ts-expect-error Server Component */}
-                    <RecentTweets />
-                </Suspense>
-            </div>
-            <div className="mb-2">
-                <H2 className="font-semibold">Last Liked Tweet</H2>
-                <Suspense fallback={<TweetSkeleton />}>
-                    {/* @ts-expect-error Server Component */}
-                    <LikedTweets />
-                </Suspense>
             </div>
         </div>
     )
